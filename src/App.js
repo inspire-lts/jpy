59d0677557fb4ff96acc7949e478c8113cf109db
inspire-lts <1183132919@qq.com>
2020-11-24 14:27:29 +0800

    图片上传

--- src/App.js
+++ src/App.js
@@ -3,20 +3,53 @@ import { Chrono} from 'react-chrono'
 
 const Home = () => {
   const items = [{
-    title: "May 1940",
-    cardTitle: "Dunkirk",
-    cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+    title: "小时候",
     media: {
       type: "IMAGE",
       source: {
-        url: "http://someurl/image.jpg"
+        url: "https://gitee.com/new_lts/MyImages/raw/master/img/QQ图片20201123140229.jpg"
+      }
+    },
+  },{
+    title: "高中",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://gitee.com/new_lts/MyImages/raw/master/img/1.jpg"
+      }
+    },
+  }, {
+    title: "大学",
+    media:  {
+      type: "IMAGE",
+      source: {
+        url: "https://gitee.com/new_lts/MyImages/raw/master/img/3.jpg"
       }
     }
-  }];
+  },{
+    title: "相遇",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "https://gitee.com/new_lts/MyImages/raw/master/img/QQ图片20201124135833.jpg"
+      }
+    }
+  }, {
+    title: "别跑",
+    media: {
+      tyoe: "IMAGE",
+      source: {
+        url: "https://gitee.com/new_lts/MyImages/raw/master/img/4.jpg"
+      }
+    }
+  } ];
 
   return (
-    <div style={{ width: "500px", height: "400px" }}>
-      <Chrono items={items} />
+    <div>
+      <Chrono 
+      items={items}
+      slideShow
+      mode='VERTICAL_ALTERNATING'/>
     </div>
   )
 }
